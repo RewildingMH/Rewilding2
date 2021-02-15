94aca21e5da276cfaa143c0bb6e6831a70c7c643
unknown <lucas_e_93@hotmail.com>
2021-02-15 00:26:03 -0300

    comments

--- frontend/src/Components/Petition.jsx
+++ frontend/src/Components/Petition.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import petitionsActions from "../redux/actions/PetitionsActions";
-import { Reasons } from "./Reasons.jsx";
+import { Reasons } from './Reasons';
+
 
 const Petition = (props) => {
   const [signature, setSignature] = useState({});
@@ -11,12 +12,12 @@ const Petition = (props) => {
   const id = props.match.params.id;
 
   const { onePetition } = props;
-
+  console.log(props.onePetition)
   useEffect(() => {
     setPetition(onePetition.filter((petition) => petition._id === id));
     props.onePetition.length === 0 && props.history.push("/petitions");
     props.getReason();
-  }, [id]);
+  }, [id, onePetition]);
 
   const readInput = (e) => {
     const reason = e.target.value;
@@ -27,6 +28,7 @@ const Petition = (props) => {
     });
   };
 
+  
   const signPetition = async () => {
     if (props.loggedUser) {
       setReload(!reload);
@@ -35,10 +37,10 @@ const Petition = (props) => {
       alert("must be logged");
     }
   };
+
   return (
     <div>
       <h2>Petition: {petition.length ? petition[0].title : ""}</h2>
-
       <input
         type="text"
         onChange={readInput}
@@ -46,18 +48,14 @@ const Petition = (props) => {
       />
       <button onClick={signPetition}>Sign petition</button>
       <h4>Signatures</h4>
-      {petition.length && props.reasons.length
-        ? props.reasons.map((reason) => <Reasons reason={reason} />)
-        : ""}
+      {petition.length && <Reasons reasons={petition[0].reasons} />}
     </div>
-  );
-};
+)}
 
 const mapStateToProps = (state) => {
   return {
     loggedUser: state.authR.loggedUser,
     onePetition: state.petitionsR.allPetitions,
-    reasons: state.petitionsR.reasons,
   };
 };
 
