49cffff1cb9944c0bb0bec007cecc75f7063f23c
unknown <lucas_e_93@hotmail.com>
2021-02-21 18:37:11 -0300

    asd

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,29 +1,30 @@
 const User = require("../models/User")
 const nodemailer = require('nodemailer')
+const smtpTransport = require('nodemailer-smtp-transport')
 const emailController = {
     recoverPasword: async (req, res) => {
         try{
             const {email } = req.body.email
             const response = await User.findOne({username: email})
        
-            const transporter = nodemailer.createTransport({
-               service: "Gmail",
+            const transporter = nodemailer.createTransport(smtpTransport({
+               service: "gmail",
                 auth: {
-                  user: "lucaasse93267@gmail.com", // generated ethereal user
-                  pass: "12031203l", // generated ethereal password
+                  user: "rewinding.squad@gmail.com", // generated ethereal user
+                  pass: "rewilding2021", // generated ethereal password
                 },
-            })
+            }))
             if(response !== null){
-                const info = transporter.sendMail({
+                const data = transporter.sendMail({
                     from: "REWILDING MINDHUB", // sender address
                     to: response.username, // list of receivers
                     subject: "Hello ✔", // Subject line
-                    text: "NO QUIERO QUE ME LLEGUE EL HELLO WORD", // plain text body
+                    text: "NO QUIERO QUE LLEGUE EL HELLO WORD", // plain text body
                     html: "<img src='https://d1hoh05jeo8jse.cloudfront.net/media/google/gmail-icon.jpg'><strong>test</strong>El nombre es ${nombre} <br />con el correo ${correo} y El mensaje es ${mensaje}, ah y el tema es ${tema}`, // html body"
-                  });
+                  })
                  res.json({
                      success: true,
-                    info: info.messageId,
+                    info: data.messageId,
                     response
                  })
             }else{
