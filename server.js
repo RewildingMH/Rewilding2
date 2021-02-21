f2e2336fd0fda38835b0225122f07bb39163f303
unknown <lucas_e_93@hotmail.com>
2021-02-21 16:37:53 -0300

    asd

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.use('/api', router)
 // })
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('/client/build'))
+    app.use(express.static('client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+'/client/build/index.html'))
+        res.sendFile(path.join(__dirname+'client/build/index.html'))
     } )
 }
 
